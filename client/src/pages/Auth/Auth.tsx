0fcdf9b6d4cea26258f3b7af78172d7f13e6b566
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-09-30 20:28:03 +0300

    feat: add react-hook-form

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,37 +1,79 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from './Auth.module.css';
 
 function Auth() {
   const [activePanel, setActivePanel] = useState(false);
   const handleChangeActive = () => {
     setActivePanel((prev) => !prev);
-    console.log('+');
   };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className={`${styles.container} ${activePanel ? `${styles.right_panel_active}` : ''}`}>
       <div className={`${styles.form_sign_up} ${styles.form_container}`}>
-        <form action="#" className={styles.auth_form}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.auth_form}>
           <h2 className={`${styles.auth_title} ${styles.title_margin}`}>Create Account</h2>
           <p className={styles.auth_text}>or use your email for registration</p>
           <div className={styles.form_direction}>
-            <input className={styles.auth_input} type="text" placeholder="Name" />
-            <input className={styles.auth_input} type="email" placeholder="Email" />
-            <input className={styles.auth_input} type="password" placeholder="Password" />
+            <input
+              className={styles.auth_input}
+              type="text"
+              {...register('name')}
+              placeholder="Name"
+            />
+            <input
+              className={styles.auth_input}
+              type="email"
+              {...register('email')}
+              placeholder="Email"
+            />
+            <input
+              className={styles.auth_input}
+              type="password"
+              {...register('password')}
+              placeholder="Password"
+            />
           </div>
-          <button className={styles.auth_button}>Sign Up</button>
+          <button type="submit" className={styles.auth_button}>
+            Sign Up
+          </button>
         </form>
       </div>
 
-      <div className={`${styles.form_sign_in} ${styles.form_container}`}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`${styles.form_sign_in} ${styles.form_container}`}>
         <h2 className={`${styles.auth_title} ${styles.title_margin}`}>Sign in</h2>
         <p className={styles.auth_text}>or use your account</p>
         <div className={styles.form_direction}>
-          <input className={styles.auth_input} type="email" placeholder="Email" />
-          <input className={styles.auth_input} type="password" placeholder="Password" />
+          <input
+            className={styles.auth_input}
+            type="email"
+            {...register('email')}
+            placeholder="Email"
+          />
+          <input
+            className={styles.auth_input}
+            type="password"
+            {...register('password')}
+            placeholder="Password"
+          />
         </div>
         <span className={`${styles.auth_text} ${styles.span_margin}`}>forgot your password?</span>
-        <button className={styles.auth_button}>Sign In</button>
-      </div>
+        <button type="submit" className={styles.auth_button}>
+          Sign In
+        </button>
+      </form>
 
       <div className={styles.container_overlay}>
         <div className={styles.overlay}>
