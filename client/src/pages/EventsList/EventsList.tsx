3e0666b4bc07b9cf201df291aada54d33404aad1
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-03 17:58:42 +0300

    feat: add EventsList component

---
+++ client/src/pages/EventsList/EventsList.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header';
+
+import styles from './EventsList.module.css';
+
+function EventsList() {
+  return (
+    <>
+      <Header />
+    
+    </>
+  )
+}
+
+export default EventsList;
