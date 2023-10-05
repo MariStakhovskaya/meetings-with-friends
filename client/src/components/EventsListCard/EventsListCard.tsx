8623324204993439d77df1bba7b4c7855228813c
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-05 11:51:07 +0300

    add EventsListCard component

--- client/src/components/EventsListCard/EventsListCard.tsx
+++ client/src/components/EventsListCard/EventsListCard.tsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from 'react';
+
 import styles from './EventsListCard.module.css';
+
+import stub from '../../assets/stub.png';
+import icon from '../../assets/iconHeart.png';
 
 type EventsListCardType = {
   title: string;
-  button: string;
-  color: boolean;
+  img: string;
+  checked: boolean;
+  data: string;
+  sort: string;
+  sortCard: string;
 };
 
-function EventsListCard({ title, button, color }: EventsListCardType) {
+function EventsListCard({ title, img, checked, data, sort, sortCard }: EventsListCardType) {
+  const [classStyle, setClass] = useState('');
+
+  useEffect(() => {
+    if (sortCard === 'world') {
+      setClass('world');
+    }
+    if (sortCard === 'events') {
+      setClass('event');
+    }
+    if (sortCard === 'meeting') {
+      setClass('meeting');
+    }
+  }, []);
+
   return (
-    <div className={`${styles.card} ${color ? `${styles.card_orange}` : `${styles.card_yellow}`}`}>
-      <div className={styles.card_text}>
-        <h3>{title}</h3>
-        <button>{button}</button>
+    <div className={styles.card_container}>
+      <div className={styles.card_img}>
+        {checked && <img src={icon} alt="Icon" className={styles.icon} />}
+        <img src={img === '' ? stub : img} alt="Event image" className={styles.img} />
+      </div>
+      <div className={styles.margin}>
+        <div className={styles.card_text}>
+          <p>{data}</p>
+          <p className={classStyle}>{sort}</p>
+        </div>
+        <p>{title}</p>
       </div>
     </div>
   );
