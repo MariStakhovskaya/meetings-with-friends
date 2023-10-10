5576acdeb0e44af72acfcc9e7efbe27431a1b2c5
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-10 15:44:03 +0300

    feat: add new page-component oneevent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Auth from './pages/Auth/Auth'
-import OneEvent from './pages/Event/OneEvent'
+import OneEvent from './pages/OneEvent/OneEvent'
 import EventsList from './pages/EventsList/EventsList'
 import Home from './pages/Home/Home'
 
